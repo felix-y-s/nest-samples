c7628368fcb347bfc706ec7822d26c1fb74050d0
ohjsda <codeozxxx@gmail.com>
2025-10-21 16:54:33 +0900

    feat: 공통 라이브러리(dto) 추가

--- libs/shared/src/dto/user.dto.ts
+++ libs/shared/src/dto/user.dto.ts
@@ -1,5 +1,41 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UserDto {
-  
-  id: number;
-  name: string;
+  @IsEmail({}, { message: '유효한 이메일 주소를 입력해주세요' })
+  @IsNotEmpty({ message: '이메일은 필수입니다' })
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '비밀번호는 필수입니다' })
+  @MinLength(8, { message: '비밀번호는 최소 8자 이상이어야 합니다' })
+  @MaxLength(20, { message: '비밀번호는 최대 20자 이하여야 합니다' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message: '비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다',
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '닉네임은 필수입니다' })
+  @MinLength(2, { message: '닉네임은 최소 2자 이상이어야 합니다' })
+  @MaxLength(20, { message: '닉네임은 최대 20자 이하여야 합니다' })
+  nickname: string;
+
+  @IsString()
+  @IsOptional()
+  @Matches(/^01[0-9]{8,9}$/, {
+    message: '유효한 전화번호를 입력해주세요 (예: 01012345678)',
+  })
+  phoneNumber?: string;
 }
